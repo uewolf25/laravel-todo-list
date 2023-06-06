1a29878ce7f51929063f38ae68ab8e3f80bc7042
wolf <nyan643.ym25mm10@gmail.com>
2023-06-06 15:18:42 +0900

    add: create table, display localpage

--- laravel/app/routes/web.php
+++ laravel/app/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TodoListController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/list', [TodoListController::class, 'index']);
